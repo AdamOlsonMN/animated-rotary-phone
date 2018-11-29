70a68cad2ac47bbdd4ca6d57b9514a7ffa46f0e2
Adam Olson <olso4075@umn.edu>
2018-11-28 20:53:26 -0600

    working on cleaning

--- DataImportClean.py
+++ DataImportClean.py
@@ -5,6 +5,10 @@ import numpy as np
 
 df = pd.read_csv('./plays.csv')
 # add in team here
-plays = ['Date','GameID','qtr','time','yrdline100','PlayType','FieldGoalResult','FieldGoalDistance','posteam','DefensiveTeam','PosTeamScore','DefTeamScore','Season']
-plays = df[plays]
-# https://www.kaggle.com/gnarlyinsights/nfl-analysis-predicting-play-type
+play_attr = ['GameID','qtr','TimeSecs','yrdline100','ydstogo','Drive','down','PlayType','PassAttempt','RushAttempt','Yards.Gained','posteam','DefensiveTeam','PosTeamScore','DefTeamScore','Season']
+plays = df[play_attr]
+
+plays = plays[plays.PlayType.notna() & (plays.PlayType != 'No Play') & (plays.PlayType != 'Kickoff') & (plays.PlayType != 'Extra Point')]
+plays=plays.rename(columns = {'posteam':'Team'})
+
+
