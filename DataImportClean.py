d290f4f9b5ab70ddd949fb9786b1207e89e1b9b7
Adam Olson <olso4075@umn.edu>
2018-11-28 19:39:18 -0600

    working on cleaning

--- DataImportClean.py
+++ DataImportClean.py
@@ -1,5 +1,10 @@
 # Essentials: Data Cleansing and ETL
 import pandas as pd
+
 import numpy as np
 
-df = pd.read_csv('../NFL Play by Play 2009-2017 (v4).csv')
+df = pd.read_csv('./plays.csv')
+# add in team here
+plays = ['Date','GameID','qtr','time','yrdline100','PlayType','FieldGoalResult','FieldGoalDistance','posteam','DefensiveTeam','PosTeamScore','DefTeamScore','Season']
+plays = df[plays]
+# https://www.kaggle.com/gnarlyinsights/nfl-analysis-predicting-play-type
