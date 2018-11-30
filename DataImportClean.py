14b5562621753a9a38499f2859f7cc49c4b5d9a2
Adam Olson <olso4075@umn.edu>
2018-11-29 21:21:20 -0600

    reformatted code

--- DataImportClean.py
+++ DataImportClean.py
@@ -3,12 +3,32 @@ import pandas as pd
 
 import numpy as np
 
-df = pd.read_csv('./plays.csv')
+df = pd.read_csv("./plays.csv")
 # add in team here
-play_attr = ['GameID','qtr','TimeSecs','yrdline100','ydstogo','Drive','down','PlayType','PassAttempt','RushAttempt','Yards.Gained','posteam','DefensiveTeam','PosTeamScore','DefTeamScore','Season']
+play_attr = [
+    "GameID",
+    "qtr",
+    "TimeSecs",
+    "yrdline100",
+    "ydstogo",
+    "Drive",
+    "down",
+    "PlayType",
+    "PassAttempt",
+    "RushAttempt",
+    "Yards.Gained",
+    "posteam",
+    "DefensiveTeam",
+    "PosTeamScore",
+    "DefTeamScore",
+    "Season",
+]
 plays = df[play_attr]
 
-plays = plays[plays.PlayType.notna() & (plays.PlayType != 'No Play') & (plays.PlayType != 'Kickoff') & (plays.PlayType != 'Extra Point')]
-plays=plays.rename(columns = {'posteam':'Team'})
-
-
+plays = plays[
+    plays.PlayType.notna()
+    & (plays.PlayType != "No Play")
+    & (plays.PlayType != "Kickoff")
+    & (plays.PlayType != "Extra Point")
+]
+plays = plays.rename(columns={"posteam": "Team"})
